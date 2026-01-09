fed65421cffb5e604ebc21298756fcbf41a0be58
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-09 19:52:41 +0530

    Started the first server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/" , (req , res)=>{
+    return res.send("<h2>Hi there </h2>")
+})
+
+app.listen(port , ()=>{
+    console.log(`Listening to the port : ${port}`);
+})
