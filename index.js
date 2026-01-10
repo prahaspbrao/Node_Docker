36869ce16b9dacaedaffb449d209331bf3e6ec91
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-10 19:32:36 +0530

    Added 3 more docker compose file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/" , (req , res)=>{
-    return res.send("<h2>Hello SKanda Rao P Gwdkbekfvb efkvjb efkvjbekvfb</h2>")
+    return res.send("<h2>Hello World</h2>")
 })
 
 app.listen(port , ()=>{
