5767dcb148ea163f94ae0688720c540a86f50e87
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-12 18:03:33 +0530

    Added values of mongo connect

---
+++ config/config.js
@@ -0,0 +1,6 @@
+export default {
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MNGO_PORT: process.env.MNGO_PORT || 27017,
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import {MONGO_USER , MONGO_IP , MNGO_PORT} from "./config/config.js"
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-await mongoose.connect("mongodb://prahas:mypassword@mongo:27017/?authSource=admin")
+await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MNGO_PORT}/?authSource=admin`)
 .then(() => console.log("Successfully connected to database"))
 .catch((e) => console.log(e))
 
