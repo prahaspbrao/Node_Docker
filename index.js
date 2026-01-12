b6cd65376d973e475ff456db43690a7ed6f224b7
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-12 23:54:09 +0530

    Completed seting up docker

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ import express from "express";
 import mongoose from "mongoose";
 import config from "./config/config.js";
 
-const {
-  MONGO_USER,
-  MONGO_IP,
-  MONGO_PORT,
-  MONGO_PASSWORD,
-} = config;
+const { MONGO_USER, MONGO_IP, MONGO_PORT, MONGO_PASSWORD } = config;
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-await mongoose.connect(mongoURL )
-.then(() => console.log("Successfully connected to database"))
-.catch((e) => console.log(e))
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("Successfully connected to database"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
-app.get("/" , (req , res)=>{
-    return res.send("<h2>Hello World !!!</h2>")
-})
+app.get("/", (req, res) => {
+  return res.send("<h2>Hello World !!!</h2>");
+});
 
-app.listen(port , ()=>{
-    console.log(`Listening to the port : ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Listening to the port : ${port}`);
+});
