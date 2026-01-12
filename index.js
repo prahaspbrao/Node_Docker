770b88de918f3fe35562424ae20cd69e56b3cf08
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-01-12 17:44:06 +0530

    IMported mondoose and successfully connected to the docker

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+await mongoose.connect("mongodb://prahas:mypassword@172.18.0.2:27017/?authSource=admin")
+.then(() => console.log("Successfully connected to database"))
+.catch((e) => console.log(e))
 
 app.get("/" , (req , res)=>{
     return res.send("<h2>Hello World !!!</h2>")
